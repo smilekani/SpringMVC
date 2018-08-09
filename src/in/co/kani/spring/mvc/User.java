1be91697335460877bd7553f0610064b134ed629
Kanimozhi Mani <Kanimozhi.infinite.com>
2018-08-09 18:36:45 +0530

    first commit in Spring MVC

---
+++ src/in/co/kani/spring/mvc/User.java
@@ -0,0 +1,32 @@
+/**
+ * @author Kanimozhi.M
+ *
+ * @Dated 09-Aug-2018  5:28:43 PM
+ *
+ * @Email Kanimozhi.Mani@infinite.com
+ *
+ * @Project : Bayonette : wio
+ *
+ * @Location: in.co.kani.spring.mvc.User.java
+ *
+ *
+ */
+package in.co.kani.spring.mvc;
+
+/**
+ * @author Kanimozhi.M
+ *
+ * @Dated 09-Aug-2018  5:28:43 PM
+ *
+ */
+public class User {
+	private String userName;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+}
